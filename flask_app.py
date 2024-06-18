7279e41426e4340804a36c9898ca5bacdb74d22f
munich2024 <166804953+munich2024@users.noreply.github.com>
2024-06-18 09:55:22 +0200

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -9,27 +9,22 @@ PYTHONANYWHERE_WEBAPPNAME = "mysite"
 app = Flask(__name__)
 
 my_type_role = """
-    As a digital therapy coach, check in daily with your patient to assess their well-being related to their chronic condition.
-    Use open-ended questions and empathetic dialogue to create a supportive environment.
-    Reflectively listen and encourage elaboration to assess the patient's detailed condition without directing the topic.
+    Du bist ein Assistenz-Chatbot namens Remindo, spezialisiert darauf, Nutzern zu helfen, sich an Dinge zu erinnern, die ihnen auf der Zunge liegen, aber momentan nicht abgerufen werden können. Du agierst als intelligentes Werkzeug und führst dynamische Gesprächsinteraktionen, um das gesuchte Wissen effektiv hervorzurufen. Dein Ziel ist es, die Nutzer durch gezielte Fragen, Hinweise und Gedächtnisstützen zu unterstützen, damit sie selbst auf die gesuchten Informationen kommen.
 """
 
 my_instance_context = """
-    Meet Daniel Müller, 52, who is tackling obesity with a therapy plan that includes morning-to-noon intermittent fasting, 
-    thrice-weekly 30-minute swims, and a switch to whole grain bread.
+    
 """
 
 my_instance_starter = """
-Jetzt, frage nach dem Namen und einem persönlichen Detail (z.B. Hobby, Beruf, Lebenserfahrung).
-Verwende diese im geschlechtsneutralem Gespräch in Du-Form.
-Sobald ein Name und persönliches Detail bekannt ist, zeige eine Liste von Optionen.
+Begrüsse den User höflich und stelle dich als Remindo vor.
 """
 
 bot = Chatbot(
     database_file="database/chatbot.db", 
     type_id="coach",
     user_id="daniel",
-    type_name="Health Coach",
+    type_name="Remindo",
     type_role=my_type_role,
     instance_context=my_instance_context,
     instance_starter=my_instance_starter
